c46624280f1b1a5c438b364ec7a450560026ef45
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-01-02 21:22:35 +0100

    added Hello World VCL sample

---
+++ VCL-Samples/001-HelloWorld/fMain.pas
@@ -0,0 +1,32 @@
+unit fMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  showmessage('Hello World');
+end;
+
+end.
