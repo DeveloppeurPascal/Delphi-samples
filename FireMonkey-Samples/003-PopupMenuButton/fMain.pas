79b1fb6e90b866fb890eb411369fa181d1f0d787
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-01-03 17:33:32 +0100

    added FireMonkey popup menu sample

---
+++ FireMonkey-Samples/003-PopupMenuButton/fMain.pas
@@ -0,0 +1,30 @@
+unit fMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Menus,
+  FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    PopupMenu1: TPopupMenu;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
