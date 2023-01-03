82ceaa2c4705dc36dfd744d47e31f2ed634195c8
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-01-03 17:32:43 +0100

    added FireMonkey Hello World sample

---
+++ FireMonkey-Samples/001-HelloWorld/HelloWorld.dpr
@@ -0,0 +1,14 @@
+program HelloWorld;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  fMain in 'fMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ FireMonkey-Samples/001-HelloWorld/fMain.pas
@@ -0,0 +1,33 @@
+unit fMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  showmessage('Hello World');
+end;
+
+end.
