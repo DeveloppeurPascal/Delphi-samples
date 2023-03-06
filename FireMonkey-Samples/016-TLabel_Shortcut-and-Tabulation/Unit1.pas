cfb1a1697e077137c1909c4cac839a626b15c60d
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-03-06 16:40:27 +0100

    added FMX Sample of TLabel FocusControl property and shortcut

---
+++ FireMonkey-Samples/016-TLabel_Shortcut-and-Tabulation/Unit1.pas
@@ -0,0 +1,41 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Edit, FMX.Controls.Presentation;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Edit1: TEdit;
+    Label2: TLabel;
+    Edit2: TEdit;
+    Label3: TLabel;
+    Edit3: TEdit;
+    Label4: TLabel;
+    CheckBox1: TCheckBox;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  close;
+end;
+
+end.
