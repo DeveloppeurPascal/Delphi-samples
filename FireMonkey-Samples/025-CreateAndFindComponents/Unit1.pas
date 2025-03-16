eb67518c1c512cd3560089963b667d87ec00f5a3
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-03-16 18:38:55 +0100

    added a new FMX sample to create and find components by code

---
+++ FireMonkey-Samples/025-CreateAndFindComponents/Unit1.pas
@@ -0,0 +1,146 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Graphics,
+  FMX.Dialogs,
+  FMX.Layouts,
+  FMX.Controls.Presentation,
+  FMX.StdCtrls,
+  FMX.Edit;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    FlowLayout1: TFlowLayout;
+    Label1: TLabel;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Edit1Enter(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+  private
+    FNbPlusPlus: integer;
+    function GetNbPlusPlus: integer;
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+
+    property NbPlusPlus: integer read GetNbPlusPlus;
+    constructor Create(AOwner: TComponent); override;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  e: tedit;
+begin
+  e := tedit.Create(self);
+  e.parent := FlowLayout1;
+  e.text := 'self';
+  e.Name := 'Edt' + NbPlusPlus.ToString;
+  e.tagString := 'MonEdit_' + NbPlusPlus.ToString;
+  e.OnEnter := Edit1Enter;
+  e.width := FlowLayout1.width;
+
+  e := tedit.Create(nil);
+  e.parent := FlowLayout1;
+  e.text := 'nil';
+  e.Name := 'Edt' + NbPlusPlus.ToString;
+  e.tagString := 'MonEdit_' + NbPlusPlus.ToString;
+  e.OnEnter := Edit1Enter;
+  e.width := FlowLayout1.width;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  i: integer;
+begin
+  for i := 0 to ComponentCount - 1 do
+    if (components[i] is tedit) and (components[i] as tedit)
+      .tagString.endswith('_14') then
+    begin
+      (components[i] as tedit).SetFocus;
+      break;
+    end;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var
+  c: TComponent;
+begin
+  c := FindComponent('Edt5');
+  if c is tedit then
+    (c as tedit).SetFocus;
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+var
+  i: integer;
+  e: tedit;
+begin
+  for i := 0 to ComponentCount - 1 do
+    if components[i] is tedit then
+    begin
+      e := components[i] as tedit;
+      e.text := e.Name + ' - ' + e.tagString;
+    end;
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+var
+  i: integer;
+  e: tedit;
+begin
+  for i := 0 to FlowLayout1.ChildrenCount - 1 do
+    if FlowLayout1.Children[i] is tedit then
+    begin
+      e := FlowLayout1.Children[i] as tedit;
+      if e.Owner = nil then
+        e.text := 'nil (pas glop)'
+      else if e.Owner is TComponent then
+        e.text := (e.Owner as TComponent).Name
+      else
+        e.text := 'unknow owner';
+    end;
+end;
+
+constructor TForm1.Create(AOwner: TComponent);
+begin
+  inherited;
+  FNbPlusPlus := 0;
+end;
+
+procedure TForm1.Edit1Enter(Sender: TObject);
+begin
+  if Sender is tedit then
+    Label1.text := (Sender as tedit).tagString;
+end;
+
+function TForm1.GetNbPlusPlus: integer;
+begin
+  FNbPlusPlus := FNbPlusPlus + 1;
+  result := FNbPlusPlus;
+end;
+
+end.
