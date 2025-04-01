9db1f05638ec621a8578392cea3774d53f3db103
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-04-01 17:56:50 +0200

    added ToLowerInvariant and ToUpperInvariant functions

--- Console-Samples/010-StringComparison/StringComparison.dpr
+++ Console-Samples/010-StringComparison/StringComparison.dpr
@@ -36,8 +36,8 @@
 /// https://github.com/DeveloppeurPascal/Delphi-samples
 ///
 /// ***************************************************************************
-/// File last update : 2025-04-01T11:42:32.293+02:00
-/// Signature : ff595a60024b749de0b1dd1e2a918d034b8c7834
+/// File last update : 2025-04-01T17:34:38.000+02:00
+/// Signature : 22bfedba947dcea84f26bcb8532a5ef8f899b54f
 /// ***************************************************************************
 /// </summary>
 
@@ -50,41 +50,88 @@ uses
   System.SysUtils;
 
 var
-  s1, s1L, s1U, s2, s2L, s2U: string;
+  s1, s1L, s1U, s1LI, s1UI, s2, s2L, s2U, s2LI, s2UI, s3, s3L, s3U, s3LI, s3UI: string;
 
 begin
   try
     s1 := 'IciOuPas';
     s1L := s1.ToLower;
+    s1LI := s1.ToLowerInvariant;
     s1U := s1.ToUpper;
+    s1UI := s1.ToUpperInvariant;
+
     s2 := 'icioupas';
     s2L := s2.ToLower;
+    s2LI := s2.ToLowerInvariant;
     s2U := s2.ToUpper;
+    s2UI := s2.ToUpperInvariant;
+
+    s3 := 'ICIOUPAS';
+    s3L := s3.ToLower;
+    s3LI := s3.ToLowerInvariant;
+    s3U := s3.ToUpper;
+    s3UI := s3.ToUpperInvariant;
 
     writeln('s1');
     writeln(s1);
     writeln(s1L);
+    writeln(s1LI);
     writeln(s1U);
+    writeln(s1UI);
 
     writeln('s2');
     writeln(s2);
     writeln(s2L);
+    writeln(s2LI);
     writeln(s2U);
+    writeln(s2UI);
+
+    writeln('s3');
+    writeln(s3);
+    writeln(s3L);
+    writeln(s3LI);
+    writeln(s3U);
+    writeln(s3UI);
+
+    // readln;
 
     writeln('s1 = s2');
     writeln(s1 = s2);
     writeln(s1L = s2L);
     writeln(s1U = s2U);
+    writeln(s1LI = s2LI);
+    writeln(s1UI = s2UI);
+
+    writeln('s1 = s3');
+    writeln(s1 = s3);
+    writeln(s1L = s3L);
+    writeln(s1U = s3U);
+    writeln(s1LI = s3LI);
+    writeln(s1UI = s3UI);
 
     writeln('CompareStr');
-    writeln(comparestr(s1, s2));
-    writeln(comparestr(s1L, s2L));
-    writeln(comparestr(s1U, s2U));
+    writeln(compareStr(s1, s2));
+    writeln(compareStr(s1L, s2L));
+    writeln(compareStr(s1U, s2U));
+    writeln(compareStr(s1LI, s2LI));
+    writeln(compareStr(s1UI, s2UI));
+    writeln(compareStr(s1, s3));
+    writeln(compareStr(s1L, s3L));
+    writeln(compareStr(s1U, s3U));
+    writeln(compareStr(s1LI, s3LI));
+    writeln(compareStr(s1UI, s3UI));
 
     writeln('CompareText');
-    writeln(comparetext(s1, s2));
-    writeln(comparetext(s1L, s2L));
-    writeln(comparetext(s1U, s2U));
+    writeln(compareText(s1, s2));
+    writeln(compareText(s1L, s2L));
+    writeln(compareText(s1U, s2U));
+    writeln(compareText(s1LI, s2LI));
+    writeln(compareText(s1UI, s2UI));
+    writeln(compareText(s1, s3));
+    writeln(compareText(s1L, s3L));
+    writeln(compareText(s1U, s3U));
+    writeln(compareText(s1LI, s3LI));
+    writeln(compareText(s1UI, s3UI));
 
     readln;
   except
