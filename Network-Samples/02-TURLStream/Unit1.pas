ebd9577e99978fae72a6cb65d7fc3dcc39f1bb09
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-06-09 08:45:33 +0200

    added a sample use of TURLStream class

---
+++ Network-Samples/02-TURLStream/Unit1.pas
@@ -0,0 +1,98 @@
+(* C2PP
+  ***************************************************************************
+
+  Delphi Sample Projects
+
+  Copyright 1995-2025 Patrick Prémartin under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Set of projects demonstrating the features of the Delphi development
+  environment, its libraries and its programming language.
+
+  Some of the projects have been presented at conferences, on training
+  courses or online coding sessions.
+
+  The programs are up to date with the Community Edition and the commercial
+  version of Delphi or RAD Studio.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://samples.developpeur-pascal.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Delphi-samples
+
+  ***************************************************************************
+  File last update : 2025-06-09T08:42:52.000+02:00
+  Signature : 79d0d38e50eadc51d2dd78cea02e2ef5030e44b1
+  ***************************************************************************
+*)
+
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Graphics,
+  FMX.Dialogs,
+  FMX.Controls.Presentation,
+  FMX.StdCtrls,
+  FMX.Memo.Types,
+  FMX.ScrollBox,
+  FMX.Memo;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+uses
+  System.Net.HttpClient,
+  System.Net.URLClient;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Button1.Enabled := false;
+  turlstream.create('https://developpeur-pascal.fr/index.html',
+    procedure(AStream: TStream)
+    begin
+      Memo1.lines.LoadFromStream(AStream);
+      Button1.Enabled := true;
+    end);
+end;
+
+end.
