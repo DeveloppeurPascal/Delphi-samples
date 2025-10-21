16a7b108aca78e4b7999492e6c45a8242d189b88
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-21 16:52:41 +0200

    moved Web-Samples\013-TMSWebCore-HelloWorld sample to an other folder

---
+++ 3rdParty-Samples/TMSSoftware/013-TMSWebCore-HelloWorld/HelloWorld.dpr
@@ -0,0 +1,58 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Delphi Sample Projects
+
+  Copyright 1995-2025 Patrick Prémartin under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Set of projects demonstrating the features of the Delphi development
+  environment, its libraries and its programming language.
+
+  Some of the projects have been presented at conferences, on training
+  courses or online coding sessions.
+
+  The programs are up to date with the Community Edition and the commercial
+  version of Delphi or RAD Studio.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://samples.developpeur-pascal.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Delphi-samples
+
+  ***************************************************************************
+  File last update : 2025-02-09T11:12:21.071+01:00
+  Signature : d31e0e6ec27356e81ebcc9fec5705bdcac7db60d
+  ***************************************************************************
+*)
+
+program HelloWorld;
+
+uses
+  Vcl.Forms,
+  WEBLib.Forms,
+  PageWeb in 'PageWeb.pas' {Form1: TWebForm} {*.html};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
